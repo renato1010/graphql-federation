47a053b1c9a7e4e2c6eda828ffb295cd3c924e64
renato.perez <renatoperezc@gmail.com>
2021-07-12 20:25:38 -0600

    Try query for Account entity

--- server/src/services/accounts/resolvers.ts
+++ server/src/services/accounts/resolvers.ts
@@ -1,7 +1,17 @@
-
+const accounts = [
+  {
+    id: "1",
+    email: "devchirps@mandiwise.com",
+  },
+];
 
 export const resolvers = {
+  Account: {
+    __resolveReference(reference: { id: string }, context: any, info: any) {
+      return accounts.find((account) => account.id === reference.id);
+    },
+  },
   Query: {
-    hello: () => "world",
+    viewer: () => accounts[0],
   },
 };
--- server/src/services/accounts/typeDefs.ts
+++ server/src/services/accounts/typeDefs.ts
@@ -1,7 +1,11 @@
 import { gql } from "apollo-server";
 
 export const typeDefs = gql`
+  type Account @key(fields: "id") {
+    id: ID!
+    email: String!
+  }
   extend type Query {
-    hello: String
+    viewer: Account
   }
 `;
