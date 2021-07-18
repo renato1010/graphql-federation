cc231996bd6162fbc6c43513a162661c93b61057
renato.perez <renatoperezc@gmail.com>
2021-07-17 20:28:27 -0600

    Fix deps

---
+++ server/src/lib/getToken.ts
@@ -0,0 +1,36 @@
+import { URLSearchParams } from "url";
+import fetch from "node-fetch";
+
+export const getToken = async (username: string, password: string) => {
+  const params = new URLSearchParams();
+  if (
+    !process.env.AUTH0_AUDIENCE ||
+    !process.env.AUTH0_CLIENT_ID_GRAPHQL ||
+    !process.env.AUTH0_CLIENT_SECRET_GRAPHQL
+  ) {
+    throw new Error("env vars not loaded");
+  }
+  params.append("audience", process.env.AUTH0_AUDIENCE);
+  params.append("client_id", process.env.AUTH0_CLIENT_ID_GRAPHQL);
+  params.append("client_secret", process.env.AUTH0_CLIENT_SECRET_GRAPHQL);
+  params.append("grant_type", "http://auth0.com/oauth/grant-type/password-realm");
+  params.append("realm", "Username-Password-Authentication");
+  params.append("scope", "openid");
+  params.append("password", password);
+  params.append("username", username);
+  try {
+    const response = await fetch(`https://${process.env.AUTH0_DOMAIN}/oauth/token`, {
+      method: "POST",
+      body: params,
+    });
+
+    const data = await response.json();
+    console.log({ data });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+(async () => {
+  await getToken("renatoperezc@gmail.com", "Rp101010");
+})();
